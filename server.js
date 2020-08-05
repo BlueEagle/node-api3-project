4124cd2da6febdc6f686560802c2fa43ec2ba55d
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-08-05 13:42:38 -0600

    added logger function

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+server.use(logger);
+server.use(helmet());
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `Client: ${req.ip} has made a ${req.method} request to ${req.path}`
+  );
+  next();
+}
 
 module.exports = server;
