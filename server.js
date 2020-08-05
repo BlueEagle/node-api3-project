8ad4ed511e9c811755e9071b34b2b41994243862
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-08-05 14:24:00 -0600

    post router complete

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const helmet = require("helmet");
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
 server.use(express.json());
-server.use(logger);
 server.use(helmet());
+server.use(logger);
+
+server.use("/posts", postRouter);
+server.use("/users", userRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
