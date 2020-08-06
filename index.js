c90aed564fa5fb50b9fca27434c77f4933dbb60c
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-08-06 13:54:37 -0600

    changed code for dynamic port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require("./server");
 
-const port = platform.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () =>
   console.log(`Server listening for connection at localhost:8000`)
